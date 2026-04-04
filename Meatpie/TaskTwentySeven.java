37ce34496ad0a39e7fd24aec9ae759ef1307b8ab
Joel Ogor <ogorjoelchukwuebuka@gmail.com>
2026-04-04 19:04:29 +0100

    Meat Pie

---
+++ Meatpie/TaskTwentySeven.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TaskTwentySeven {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+    
+
+        int  theNumber  = 78;
+
+        double theOutput = theNumber;
+
+       System.out.println(theNumber);
+       System.out.println(theOutput);
+    
+    }
+}
