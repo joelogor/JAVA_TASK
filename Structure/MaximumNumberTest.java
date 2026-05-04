60653f00e6216028ffe3e487e8f4e9b0368bffe2
joelogor <ogorjoelchukwuebuka@gmail.com>
2026-05-04 11:44:37 +0100

    Clean up MaximumNumberTest by removing tests
    Removed redundant test cases for maximum number.

--- Structure/MaximumNumberTest.java
+++ Structure/MaximumNumberTest.java
@@ -10,19 +10,6 @@ public class MaximumNumberTest{
         int actualNumber = 3;
         assertEquals(expectedNumber, actualNumber);
 
-        int expectedNumber1 = MaximumNumber.max(3,2,1);
-        int actualNumber1 = 3;
-        assertEquals(expectedNumber1, actualNumber1);
-
-        int expectedNumber2 = MaximumNumber.max(2,2,2);
-        int actualNumber2 = 2;
-        assertEquals(expectedNumber, actualNumber);
-
-        int expectedNumber3 = MaximumNumber.max(-1,-2,-3);
-        int actualNumber3 = -1;
-        assertEquals(expectedNumber3, actualNumber3);
-
-
 
     }   
 
