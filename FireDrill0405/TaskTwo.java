ac5e308dac2b9752653b89e8781f0d3ce8c02685
joelogor <ogorjoelchukwuebuka@gmail.com>
2026-05-14 06:23:24 +0100

    "Update OshoFreePromoEngine"

--- FireDrill0405/TaskTwo.java
+++ FireDrill0405/TaskTwo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TaskThre {
+public class TaskTwo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] numbers = new int[10];
@@ -8,10 +8,11 @@ public class TaskThre {
         for (int count = 0; count < numbers.length; count++) {
             System.out.print("Enter number: ");
             numbers[count] = input.nextInt();
+            System.out.println(numbers[count] );
         }
-        for (int count = 0; count < numbers.length; count++) {
-            System.out.print(numbers[count] + " ");
-        }
+//        for (int count = 0; count < numbers.length; count++) {
+//            System.out.print(numbers[count] + " ");
+ //       }
     }
 }
 
